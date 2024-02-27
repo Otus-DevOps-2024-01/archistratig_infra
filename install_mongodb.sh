b0906a902500878b176d32a762dee73330a72a75
archistratig <60445647+archistratig@users.noreply.github.com>
2024-02-27 14:44:36 +0300

    Update install_mongodb.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+sudo apt-get install dbus -y
 sudo apt install -y software-properties-common gnupg apt-transport-https ca-certificates -y
 sudo apt update
 sudo apt install -y apt-utils
-sudo apt-get install dbus
 sudo wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add - && \
 sudo echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list && \
 sudo apt-get update && sudo apt-get install -y mongodb-org
+sudo service mongod stop
 sudo systemctl start mongod &&  sudo systemctl enable mongod
 mongo --eval 'db.runCommand({ connectionStatus: 1 })'
