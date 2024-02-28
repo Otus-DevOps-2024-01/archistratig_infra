15a0bd35a5173dc54811492e92b942d4b03cb330
archistratig <60445647+archistratig@users.noreply.github.com>
2024-02-28 19:34:49 +0300

    Update install_mongodb.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo apt update
+sudo apt install -y software-properties-common gnupg apt-transport-https ca-certificates -y
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt update; sudo apt-get install -y mongodb-org
