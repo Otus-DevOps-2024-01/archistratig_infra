cf96a4b592b0e79517038f962be7f6345b76d632
archistratig <60445647+archistratig@users.noreply.github.com>
2024-02-28 19:50:46 +0300

    Update install_mongodb.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+hostnamectl
 sudo apt update
-sudo apt install -y software-properties-common gnupg apt-transport-https ca-certificates -y
+sudo apt install -y software-properties-common gnupg curl apt-transport-https ca-certificates -y
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.2.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-4.2.gpg \
+   --dearmor
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt update; sudo apt-get install -y mongodb-org
-if [ $? -eq 0 ]; then
-echo "MongoDB was installed" >> /tmp/log_deploy.log; sudo systemctl start mongod
-else echo "WARN: MongoDB wasn't installed" >> /tmp/log_deploy.log exit 1; fi
-exit 0;
+sudo systemctl start mongod
+sudo systemctl daemon-reload
+sudo systemctl start mongod
