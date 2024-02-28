2afb5396bd9d41c8da2e9f53e577ffe37f72c5fc
archistratig <60445647+archistratig@users.noreply.github.com>
2024-02-28 19:28:56 +0300

    Update install_mongodb.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-sudo apt update
-sudo apt install -y software-properties-common gnupg apt-transport-https ca-certificates -y
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-sudo echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-sudo apt-get update && sudo apt-get install -y mongodb-org && sudo systemctl unmask mongodb
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo mkdir -p /data/db
 sudo chown -R `id -u` /data/db
 sudo chmod -R go+w /data/db
-sudo systemctl start mongod && systemctl enable --now mongod
-mongo
+sudo apt update; sudo apt-get install -y mongodb-org && sudo systemctl start mongod
+if [ $? -eq 0 ]; then
+echo "MongoDB was installed" >> /tmp/log_deploy.log; sudo systemctl start mongod
+else echo "WARN: MongoDB wasn't installed" >> /tmp/log_deploy.log exit 1; fi
+exit 0;
